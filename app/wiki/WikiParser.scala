175c43109d51782c9f2416f99417f1fc237ad531
Tim Porter <portergoodness@gmail.com>
2014-06-07 16:09:56 -0600

    Issue #5 - Changed the level of detail the parser regular expressions was using.  Now looking at matches one character at a time.

--- app/wiki/WikiParser.scala
+++ app/wiki/WikiParser.scala
@@ -13,39 +13,80 @@ object WikiParser {
  * This class is the parser
  * It consists or rules for parsing.
  * 
- * Our Grammer is:
- * 
- * Article ::= ArticlePart { ArticlePart}
- * ArticlePart ::= Special | Heading | Link | ArticleText | Comment
- * Special ::= "{{" SpecialContent+ "}}"
- * SpecialContent ::= SpecialText | Special
- * SpecialText ::= [^}}]
- * ArticleText ::= //no starting tokens
  */
 class WikiParser extends JavaTokenParsers {
   
   override val skipWhitespace = false
   
-  def article: Parser[List[WikiPart]] = rep1(articlePart) 
-  def articlePart: Parser[WikiPart] = special | normalText  | link
+  def article: Parser[List[WikiPart]] = rep(articleContent)
   
-  //****** Special *********
-  def special: Parser[WikiSpecial] = "{{" ~> rep1(specialContent) <~ "}}" ^^ {
-    case content => WikiSpecial(content)
+//  def articleParts: Parser[List[WikiPart]] = rep1(articleContent)
+  
+  def articleContent: Parser[WikiPart] = articleText | leftBrace | leftBracket
+  
+  def articleText: Parser[WikiText] = rep1(articleTextChar) ^^ (a => WikiText(a.mkString))
+  
+  def articleTextChar: Parser[String] = """[\sa-zA-Z0-9\(\)\.,!\?:;_~'"#\^\$@%&\-\+\*\|<>\()=\\/\u00A1-\uFFFF]""".r
+  
+  
+  //****** Special - {{ Surrounded by double braces }} ******
+  def leftBrace: Parser[WikiPart] = "{" ~> insideBraces <~ "}" ^^ {
+    case content: WikiText => WikiText("{"+content.rawContent+"}")
+    case content: WikiSpecial => content
+    case content: WikiLink => WikiText("{"+content.rawContent+"}")
   }
-  def specialContent: Parser[WikiPart] = normalText | special
+  def insideBraces: Parser[WikiPart] = special | contentInBraces
+  def contentInBraces: Parser[WikiPart] = articleText | leftBracket
+  def special: Parser[WikiSpecial] = "{" ~> article <~ "}" ^^ WikiSpecial
   
-  //****** Text ******
-  def normalText: Parser[WikiText] = rep1(textParts) ^^ (a => WikiText(a.mkString))
-  def textParts: Parser[String] = word | whitespace
-  def word: Parser[String] = """[a-zA-Z0-9\(\)\.,!\?:;_~'"#\^\$@%&\-\+\*\|<>\()=\\/\u00A1-\uFFFF]+""".r
-  def whitespace: Parser[String] = """\s+""".r
   
-  //****** Link ******
-  def link: Parser[WikiLink] = "[[" ~> article <~ "]]" ^^ WikiLink
+  //****** Link - [[ Surrounded by double brackets ]] ******
+  def leftBracket: Parser[WikiPart] = "[" ~> insideBrackets <~ "]" ^^ {
+    case content: WikiText => WikiText("["+content.rawContent+"]")
+    case content: WikiSpecial => content
+    case content: WikiLink => content
+  }
+  def insideBrackets: Parser[WikiPart] = link | contentInBrackets
+  def contentInBrackets: Parser[WikiPart] = articleText | leftBrace
+  def link: Parser[WikiLink] = "[" ~> article <~ "]" ^^ WikiLink
+  
   
   //****** Parser ******
-  def parse(input: String) = parseAll(article, input)
+  def parse(input: String):Option[List[WikiPart]] = {
+    val parse = parseAll(article, input)
+    if (parse.successful) {
+      // merge any subsequent WikiText pieces.  braces and brackets can potentially split up text
+      Some(mergeWikiTexts(parse.get))
+//      Some(parse.get)
+    } else {
+      None
+    }
+  }
+  
+  def parseTest(input: String) = parseAll(article, input)
+  
+  def mergeWikiTexts(partList: List[WikiPart]): List[WikiPart] = {
+    if (partList.isEmpty) {
+      List()
+    }
+    else if (partList.size == 1) {
+      partList
+    } else {
+      val mergedList = mergeWikiTexts(partList.tail)
+      val head = partList.head
+      head match {
+        case text: WikiText => mergedList.head match {
+          case anotherText: WikiText => WikiText(text.nlContent + anotherText.nlContent) +: mergedList.tail
+          case notAnother: WikiPart => text +: mergedList
+        }
+        case other: WikiPart => mergeChildren(other) +: mergedList
+      }
+    }
+  }
+  
+  def mergeChildren(part: WikiPart) = {
+    part.setChildren(mergeWikiTexts(part.children))
+  }
 }
 
 
@@ -53,20 +94,24 @@ sealed trait WikiPart {
   def rawContent: String
   def nlContent: String
   def children: List[WikiPart]
+  def setChildren(children: List[WikiPart]): WikiPart
 }
 
 case class WikiSpecial(content: List[WikiPart]) extends WikiPart {
   def rawContent = content.map(a => a.rawContent).mkString("{{", "", "}}")
   def nlContent = ""
   def children = content
+  def setChildren(children: List[WikiPart]) = this.copy(content = children)
 }
 case class WikiLink(content: List[WikiPart]) extends WikiPart {
   def rawContent = content.map(a => a.rawContent).mkString("[[", "", "]]")
   def nlContent = content.map(a => a.nlContent).mkString
   def children = content
+  def setChildren(children: List[WikiPart]) = this.copy(content = children)
 }
 case class WikiText(content: String) extends WikiPart {
   def rawContent = content
   def nlContent = content
   def children = List()
+  def setChildren(children: List[WikiPart]) = this
 }
--- test/WikiParserSpec.scala
+++ test/WikiParserSpec.scala
@@ -1,5 +1,7 @@
 
 
+
+
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
@@ -19,9 +21,75 @@ class WikiParserSpec extends Specification {
     "parse plain text" in {
       val parseInput = "This is a test. Very Exciting! How are you? I hope that your (truly) understand; however boring it might be.  Good night, and comma..."
       val parseResults = WikiParser().parse(parseInput)
-      parseResults.successful must beTrue
+      parseResults.isDefined must beTrue
       parseResults.get.size must equalTo(1)
       parseResults.get.head.nlContent must equalTo(parseInput)
+    }
+    
+    "parse single braces as text" in {
+      val parseInput = "This is a test. {text}"
+      val parseResults = WikiParser().parse(parseInput)
+      parseResults.isDefined must beTrue
+      parseResults.get.size must equalTo(1)
+      parseResults.get.head.nlContent must equalTo(parseInput)
+    }
+    
+    "parse double braces as special" in {
+      val parseInput = "This is a test. {{text}}"
+      val expectedInput = "This is a test. "
+      val expectedSpecialRaw = "{{text}}"
+      val parseResults = WikiParser().parse(parseInput)
+      parseResults.isDefined must beTrue
+      parseResults.get.size must equalTo(2)
+      parseResults.get.head.nlContent must equalTo(expectedInput)
+      parseResults.get(1).rawContent must equalTo(expectedSpecialRaw)
+    }
+    
+    "parse braces inside double braces" in {
+      val parseInput = "This is a test. {{{text}}}"
+      val expectedInput = "This is a test. "
+      val expectedSpecialRaw = "{{{text}}}"
+      val parseResults = WikiParser().parse(parseInput)
+      parseResults.isDefined must beTrue
+      parseResults.get.size must equalTo(2)
+      parseResults.get.head.nlContent must equalTo(expectedInput)
+      val special = parseResults.get(1)
+      special.children.size must equalTo(1)
+      special.children.head.nlContent must equalTo("{text}")
+      
+    }
+    
+    "parse single brackets as text" in {
+      val parseInput = "This is a test. [text]"
+      val parseResults = WikiParser().parse(parseInput)
+      parseResults.isDefined must beTrue
+      parseResults.get.size must equalTo(1)
+      parseResults.get.head.nlContent must equalTo(parseInput)
+    }
+    
+    "parse double brackets as link" in {
+      val parseInput = "This is a test. [[text]]"
+      val expectedInput = "This is a test. "
+      val expectedSpecialRaw = "[[text]]"
+      val parseResults = WikiParser().parse(parseInput)
+      parseResults.isDefined must beTrue
+      parseResults.get.size must equalTo(2)
+      parseResults.get.head.nlContent must equalTo(expectedInput)
+      parseResults.get(1).rawContent must equalTo(expectedSpecialRaw)
+    }
+    
+    "parse brackets inside double brackets" in {
+      val parseInput = "This is a test. [[[text]]]"
+      val expectedInput = "This is a test. "
+      val expectedSpecialRaw = "[[[text]]]"
+      val parseResults = WikiParser().parse(parseInput)
+      parseResults.isDefined must beTrue
+      parseResults.get.size must equalTo(2)
+      parseResults.get.head.nlContent must equalTo(expectedInput)
+      val special = parseResults.get(1)
+      special.children.size must equalTo(1)
+      special.children.head.nlContent must equalTo("[text]")
+      
     }
     
   }
